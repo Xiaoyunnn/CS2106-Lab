a411d2f919cae1543f0feb35d2bded62c44c2b02
Wu Xiaoyun <xiaoyun.wu00@gmail.com>
2022-02-08 01:47:09 +0800

    add

--- lab2/part2/lab2p2c.c
+++ lab2/part2/lab2p2c.c
@@ -4,7 +4,10 @@
 
 int main() {
     if(fork() ==  0) {
-        execlp("cat", "cat", "file.txt", NULL);
+        char *args[] = {"cat", "file.txt", NULL};
+        // execlp("cat", "cat", "file.txt", NULL);
+        execvp("cat", args);
+
     }
     else
         wait(NULL);
