7587e428d1d9f835e02f04dac962ebbc487e5c61
Wu Xiaoyun <xiaoyun.wu00@gmail.com>
2022-02-09 12:25:53 +0800

    format lab2p2f

--- lab2/part2/lab2p2f.c
+++ lab2/part2/lab2p2f.c
@@ -4,7 +4,8 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
-int main() {
+int main()
+{
 
     printf("Be patient, the program will take around 7 seconds to run.\n");
     printf("At the end you can do \"cat results.out\" to see the result.\n");
@@ -18,24 +19,24 @@ int main() {
     int p[2];
     int fp_out = open("./results.out", O_CREAT | O_WRONLY);
 
-    if(pipe(p) < 0) 
+    if (pipe(p) < 0)
         perror("lab2p2f: ");
 
-    if(fork() != 0) {
+    if (fork() != 0)
+    {
         wait(NULL);
         dup2(p[0], STDIN_FILENO);
-        close(p[1]); 
+        close(p[1]);
         close(p[0]);
         dup2(fp_out, STDOUT_FILENO);
         execlp("./talk", "talk", NULL);
         close(fp_out);
     }
-    else {
+    else
+    {
         dup2(p[1], STDOUT_FILENO);
-        close(p[0]); 
+        close(p[0]);
         close(p[1]);
         execlp("./slow", "slow", "5", NULL);
     }
-
 }
-
