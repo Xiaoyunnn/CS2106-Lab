21809367314a9ab0fc3d30b849b3fb2195ad0788
Wu Xiaoyun <xiaoyun.wu00@gmail.com>
2022-02-09 12:15:36 +0800

    Add lab2p2f

--- lab2/part2/lab2p2f.c
+++ lab2/part2/lab2p2f.c
@@ -15,6 +15,27 @@ int main() {
     // I.e. your program should do the equivalent of ./slow 5 | talk > results.out
     // WITHOUT using | and > from the shell.
     //
+    int p[2];
+    int fp_out = open("./results.out", O_CREAT | O_WRONLY);
+
+    if(pipe(p) < 0) 
+        perror("lab2p2f: ");
+
+    if(fork() != 0) {
+        wait(NULL);
+        dup2(p[0], STDIN_FILENO);
+        close(p[1]); 
+        close(p[0]);
+        dup2(fp_out, STDOUT_FILENO);
+        execlp("./talk", "talk", NULL);
+        close(fp_out);
+    }
+    else {
+        dup2(p[1], STDOUT_FILENO);
+        close(p[0]); 
+        close(p[1]);
+        execlp("./slow", "slow", "5", NULL);
+    }
 
 }
 
