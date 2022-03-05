28a32abad496b81f579a83ceda47f00500ab36dc
Wu Xiaoyun <xiaoyun.wu00@gmail.com>
2022-03-06 00:37:20 +0800

    edit wsd

--- lab3/progs/part3/bigsmall-par.c
+++ lab3/progs/part3/bigsmall-par.c
@@ -70,7 +70,6 @@ int main() {
         }
         largest[i] = big;
         smallest[i] = small;
-        printf("start: %d, biggest: %d, smallest: %d\n", start, big, small);
         reach_barrier();
 
     }
@@ -80,7 +79,6 @@ int main() {
         start = clock();
         for(j=0; j<NUM_PROCESSES; j++)
         {
-            printf("largest[j]: %d, smallest[j]: %d\n", largest[j], smallest[j]);
             if(largest[j] > big)
                 big = largest[j];
 
