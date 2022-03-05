5ca5989e1291c8b66874bf03e1c42c8d51d9a4e2
Wu Xiaoyun <xiaoyun.wu00@gmail.com>
2022-03-05 21:32:00 +0800

    Edit lab3p1sem

--- lab3/progs/part1/lab3p1-sem.c
+++ lab3/progs/part1/lab3p1-sem.c
@@ -11,16 +11,16 @@ int main() {
 
     int i, j, k, pid, shmid;
 
-    sem_t *sem[NUM_PROCESSES];
+    sem_t sem[NUM_PROCESSES];
 
     shmid = shmget(IPC_PRIVATE, NUM_PROCESSES * sizeof(sem_t), IPC_CREAT | 0600);
     sem[0] = (sem_t *) shmat(shmid, NULL, 0);
 
     for (k = 0; k < NUM_PROCESSES; k++) {
-        sem_init(sem[k], 1, 0);
+        sem_init(&sem[k], 1, 0);
     }
 
-    sem_post(sem[0]);
+    sem_post(&sem[0]);
 
     for(i=0; i<NUM_PROCESSES; i++)
     {
@@ -30,7 +30,7 @@ int main() {
     }
 
     if(pid == 0) {
-        sem_wait(sem[i]);
+        sem_wait(&sem[i]);
         printf("I am child %d\n", i);
 
         for(j = i*10; j<i*10 + 10; j++){
@@ -40,12 +40,12 @@ int main() {
         }
 
         printf("\n\n");
-        sem_post(sem[i + 1]);
+        sem_post(&sem[i + 1]);
     }
     else {
         for(i=0; i<NUM_PROCESSES; i++) 
             wait(NULL);
-            sem_destroy(sem[i]);
+            sem_destroy(&sem[i]);
 
         shmctl(shmid, IPC_RMID, 0);
     }
