fbd3331c79175af804fad66e2d3e02e253fbe502
Wu Xiaoyun <xiaoyun.wu00@gmail.com>
2022-04-02 17:40:49 +0800

    add wf

--- lab4/linkedlist/wf/mymalloc.h
+++ lab4/linkedlist/wf/mymalloc.h
@@ -1,5 +1,8 @@
 #define MEMSIZE 64 * 1024        // Size of memory in bytes
+#define ALLOCATED "ALLOCATED"
+#define FREE "FREE"
 
 long get_index(void *ptr);
+void print_memlist();
 void *mymalloc(size_t);
 void myfree(void *);
